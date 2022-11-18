69728af405c4e19073f1bbc624b57797cf5824d5
ismaelclebson <ismaelclebson@gmail.com>
2022-11-18 16:15:58 -0300

    adionando projeto

---
+++ src/main/java/com/wassabi/model/sql/bd_wassabi.sql
@@ -0,0 +1,46 @@
+CREATE TABLE `Cliente` (
+	`cliente_id` INT(30) NOT NULL AUTO_INCREMENT,
+	`cliente_nome` varchar(80) NOT NULL,
+	`cliente_cpf` varchar(14) NOT NULL UNIQUE,
+	`cliente_endereco` varchar(80) NOT NULL,
+	`cliente_cartao` varchar(100) NOT NULL,
+	`cliente_telefone` varchar(20) NOT NULL,
+	PRIMARY KEY (`cliente_id`)
+);
+
+CREATE TABLE `Produto` (
+	`produto_id` INT(30) NOT NULL AUTO_INCREMENT,
+	`produto_nome` varchar(80) NOT NULL,
+	`produto_preco` FLOAT(30,2) NOT NULL,
+	`produto_descricao` varchar(80) NOT NULL,
+	`produto_categoria` INT(10) NOT NULL,
+	PRIMARY KEY (`produto_id`)
+);
+
+CREATE TABLE `Venda` (
+	`Venda_id` INT(30) NOT NULL AUTO_INCREMENT,
+	`Venda_cliente` INT(30) NOT NULL,
+	`Venda_total` FLOAT(30,2) NOT NULL,
+	`Venda_data` DATETIME NOT NULL,
+	PRIMARY KEY (`Venda_id`)
+);
+
+CREATE TABLE `venda_has_produto` (
+	`venda_id` INT(30) NOT NULL,
+	`produto_id` INT(30) NOT NULL,
+	`quantidade` INT(30) NOT NULL
+);
+
+CREATE TABLE `Categoria` (
+	`categoria_id` INT(30) NOT NULL AUTO_INCREMENT,
+	`categoria_nome` varchar(30) NOT NULL,
+	PRIMARY KEY (`categoria_id`)
+);
+
+ALTER TABLE `Produto` ADD CONSTRAINT `Produto_fk0` FOREIGN KEY (`produto_categoria`) REFERENCES `Categoria`(`categoria_id`);
+
+ALTER TABLE `Venda` ADD CONSTRAINT `Venda_fk0` FOREIGN KEY (`Venda_cliente`) REFERENCES `Cliente`(`cliente_id`);
+
+ALTER TABLE `venda_has_produto` ADD CONSTRAINT `venda_has_produto_fk0` FOREIGN KEY (`venda_id`) REFERENCES `Venda`(`Venda_id`);
+
+ALTER TABLE `venda_has_produto` ADD CONSTRAINT `venda_has_produto_fk1` FOREIGN KEY (`produto_id`) REFERENCES `Produto`(`produto_id`);
